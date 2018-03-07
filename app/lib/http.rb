1474a039749e1903223999e0cffbd439e4e497d7
ryym <ryym.64@gmail.com>
2018-03-07 22:36:05 +0900

    feat: Create simple Net::HTTP wrapper

---
+++ app/lib/http.rb
@@ -0,0 +1,51 @@
+require 'forwardable'
+require 'net/http'
+require 'uri'
+
+# Net::HTTP wrapper.
+class Http
+  def initialize(host, ssl: false, headers: {})
+    @host = host
+    @ssl = ssl
+    @default_headers = headers
+  end
+
+  def post_form(path, data, headers: {})
+    req = Net::HTTP::Post.new(path, @default_headers.merge(headers))
+    req.set_form_data(data)
+    res = new_http.request(req)
+    Response.new(res)
+  end
+
+  private
+
+  def new_http
+    Net::HTTP.new(@host, @ssl ? 443 : 80).tap do |http|
+      http.use_ssl = @ssl
+    end
+  end
+end
+
+class Http
+  class Response
+    extend Forwardable
+
+    delegate %i[code body] => :@res
+
+    def initialize(res)
+      @res = res
+    end
+
+    def response
+      @res
+    end
+
+    def header(key)
+      @res[key]
+    end
+
+    def form
+      @form ||= Hash[URI.decode_www_form(@res.body)]
+    end
+  end
+end
