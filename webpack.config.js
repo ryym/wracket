4cb8ef50a200f93fca99c90f14e84adbc6255517
ryym <ryym.64@gmail.com>
2018-03-22 16:40:41 +0900

    chore: Introduce TSLint and ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+/* eslint-env node */
+
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
