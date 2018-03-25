faa080411e321e21aa734a08eae3d16340041498
ryym <ryym.64@gmail.com>
2018-03-26 08:05:29 +0900

    refac(auth): Remove unused method

--- app/controllers/concerns/authenticatable.rb
+++ app/controllers/concerns/authenticatable.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 module Authenticatable
-  def pocket_authenticator
-    PocketAuthenticator.new(
-      consumer_key: ENV['POCKET_CONSUMER_KEY'],
-      redirect_uri: oauth_callback_url,
-    )
-  end
-
   def authenticate!
     # XXX: Is this secure?
     @__user = User.find_by(id: session[:user_id])
