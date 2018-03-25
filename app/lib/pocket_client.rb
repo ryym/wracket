9d00bfa314ced8d2e2e64ac87a5cf4e2d8e3596a
ryym <ryym.64@gmail.com>
2018-03-25 10:22:05 +0900

    feat(pocket): Add retrieve_each method

--- app/lib/pocket_client.rb
+++ app/lib/pocket_client.rb
@@ -23,6 +23,25 @@ class PocketClient
     ))
     res.success? ? ResRetrieve.new(res) : Pocket::ResErr.new(res)
   end
+
+  # Retrieve items for each given count and pass them to the given block.
+  # It stops the loop when a response is not a success, or the items are empty.
+  # But the given block is called on both cases.
+  def retrieve_each(count, params = {})
+    offset = 0
+    while true
+      ret = retrieve(params.merge(count: count, offset: offset))
+      if ret.err?
+        yield(ret, nil)
+        break
+      end
+
+      json = ret.response.body_json
+      yield(ret, json)
+      break if json['list'].empty?
+      offset += count
+    end
+  end
 end
 
 class PocketClient
