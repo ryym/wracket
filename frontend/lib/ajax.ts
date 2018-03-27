20dd7ab5b4a764d7d98da819b806dafeeda82a31
ryym <ryym.64@gmail.com>
2018-03-27 15:32:15 +0900

    feat(js): Create Ajax client wrapping Axios

---
+++ frontend/lib/ajax.ts
@@ -0,0 +1,53 @@
+import axios, {AxiosInstance, AxiosResponse, AxiosError} from 'axios';
+
+export type Method = 'get' | 'post' | 'put' | 'patch' | 'delete' | 'head' | 'options';
+
+export interface AjaxOption {
+  method: Method;
+  headers?: {};
+  params?: {};
+}
+
+export interface AjaxInitConfig {
+  baseURL: string;
+  headers?: {};
+}
+
+export interface Ajax {
+  <T = void>(path: string, opt: AjaxOption): Promise<Response<T>>;
+}
+
+type AxiosCreator = (conf: AjaxInitConfig) => AxiosInstance;
+const createAxios: AxiosCreator = conf => axios.create(conf);
+
+export function createAjax(conf: AjaxInitConfig, newAxios: AxiosCreator = createAxios): Ajax {
+  const axios = newAxios(conf);
+  return async function ajax<T>(path: string, opt: AjaxOption) {
+    try {
+      const res = (await axios(path, opt)) as AxiosResponse<T>;
+      return newResponse(res);
+    } catch (err) {
+      if (err.response != null) {
+        return newResponse(err.response);
+      }
+      throw err;
+    }
+  };
+}
+
+export class Response<T> {
+  readonly isSuccess: boolean;
+
+  constructor(
+    readonly data: T,
+    readonly status: number,
+    readonly statusText: string,
+    readonly headers: {} = {},
+  ) {
+    this.isSuccess = 200 <= status && status < 300;
+  }
+}
+
+function newResponse<T>(res: AxiosResponse<T>): Response<T> {
+  return new Response(res.data, res.status, res.statusText, res.headers);
+}
