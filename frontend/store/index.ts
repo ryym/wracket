2d81606589273f61190ebe317637bbaa7844b108
ryym <ryym.64@gmail.com>
2018-03-29 08:20:24 +0900

    fix(store): Allow to initialize with partial state

--- frontend/store/index.ts
+++ frontend/store/index.ts
@@ -1,4 +1,10 @@
-import {createStore, applyMiddleware, Store as ReduxStore, Dispatch as ReduxDispatch} from 'redux';
+import {
+  createStore,
+  applyMiddleware,
+  DeepPartial,
+  Store as ReduxStore,
+  Dispatch as ReduxDispatch,
+} from 'redux';
 import logger from 'redux-logger';
 import {createThunkMiddleware} from 'redux-dutiful-thunk';
 import {State} from '../state';
@@ -11,7 +17,7 @@ export type Dispatch = ReduxDispatch<Action>;
 export type Store = ReduxStore<State, Action>;
 
 export type StoreConfig = {
-  initialState?: State;
+  initialState?: DeepPartial<State>;
   context: ThunkContext;
 };
 
