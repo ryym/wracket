273de769da05664a58d9cc7aa981923d34d9222e
ryym <ryym.64@gmail.com>
2018-03-31 17:36:01 +0900

    feat(rb): Extract json creator as lib

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,12 @@
 # frozen_string_literal: true
 
 class HomeController < BaseController
-  def index
-    init = { ids: [], byId: {} }
-    bookmarks = current_user.unarchived_bookmarks.each_with_object(init) do |b, bs|
-      bs[:ids].push(b.id)
-      bs[:byId][b.id] = {
-        id: b.id,
-        title: b.entry.resolved_title,
-        url: b.entry.url,
-      }
-    end
+  before_action do
+    @json ||= JsonMaker.new
+  end
 
+  def index
+    bookmarks = @json.bookmarks(current_user.unarchived_bookmarks)
     render locals: {
       user: current_user,
       bookmarks: bookmarks,
---
+++ app/lib/json_maker.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class JsonMaker
+  # {
+  #   ids: number[],
+  #   byId: { id: number, title: string, url: string },
+  # }
+  def bookmarks(bookmarks)
+    init = { ids: [], byId: {} }
+    bookmarks.each_with_object(init) do |b, bs|
+      bs[:ids].push(b.id)
+      bs[:byId][b.id] = {
+        id: b.id,
+        title: b.entry.resolved_title,
+        url: b.entry.url,
+      }
+    end
+  end
+end
