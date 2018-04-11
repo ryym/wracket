603c379f0f04b717fb3ddd0a6de4d35958f6dd04
ryym <ryym.64@gmail.com>
2018-04-11 16:31:51 +0900

    refac(js): Allow to override asset dev server port

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 /* eslint-env node */
 
 const path = require('path');
+const dotenv = require('dotenv');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
+
+// Load environment variables from .env file.
+dotenv.config();
 
 const ROOT = __dirname;
 const FRONTEND_ROOT = path.join(ROOT, 'frontend');
@@ -12,6 +16,7 @@ const ENTRY_ROOT = path.join(FRONTEND_ROOT, 'entries');
 const DEST_DIR = path.join(ROOT, 'public', 'assets');
 
 const ENV = process.env.NODE_ENV || 'development';
+const PORT = process.env.ASSET_PORT || 3001;
 
 const byEnv = ({dev, test, prod}, defaultValue = null) => {
   switch (ENV) {
@@ -49,7 +54,7 @@ module.exports = {
 
   devServer: {
     contentBase: DEST_DIR,
-    port: 3001,
+    port: PORT,
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
