2a6e5d876cab46f22c2fc23fb89e3d238cbd5664
ryym <ryym.64@gmail.com>
2018-04-12 12:26:24 +0900

    feat(api): Return error info as json

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -6,9 +6,28 @@ module Api
 
     before_action :authenticate_json!
 
+    rescue_from StandardError, with: :handle_server_error
+
+    private
+
     def authenticate_json!
       return if auth_check_ok?
       render status: :not_found, json: { message: 'not found' }
+    end
+
+    if Rails.env.production?
+      def handle_server_error(_err)
+        render status: internal_server_error, json: {
+          message: 'Internal server error',
+        }
+      end
+    else
+      def handle_server_error(err)
+        render status: :internal_server_error, json: {
+          message: err.message,
+          backtrace: err.backtrace,
+        }
+      end
     end
   end
 end
