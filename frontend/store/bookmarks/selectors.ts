23cb768fee25d66649135479cb0a1ba0d54809b8
ryym <ryym.64@gmail.com>
2018-04-21 13:40:02 +0900

    feat(js): Memoize selector results

--- frontend/store/bookmarks/selectors.ts
+++ frontend/store/bookmarks/selectors.ts
@@ -1,13 +1,18 @@
+import {createSelector} from 'reselect';
 import {State} from '../../state';
 import {Bookmark} from '../../lib/models';
 import {iter} from '../../lib/iter';
 import {filterBookmarks, sortBookmarks} from '../../lib/bookmark-lister';
+import {getSearchCondition} from '../search/selectors';
 
-// TODO: Memoize.
-export const listBookmarks = ({bookmarks, search}: State): Bookmark[] => {
-  const bks = iter(Object.keys(bookmarks.byId))
-    .map(id => bookmarks.byId[id]!)
-    .use(filterBookmarks(search.condition))
-    .collect();
-  return sortBookmarks(bks, search.condition);
-};
+export const listBookmarks = createSelector(
+  getSearchCondition,
+  state => state.bookmarks.byId,
+  (cdtn, byId) => {
+    const bks = iter(Object.keys(byId))
+      .map(id => byId[id]!)
+      .use(filterBookmarks(cdtn))
+      .collect();
+    return sortBookmarks(bks, cdtn);
+  },
+);
