bc2a99a1bc4eea59823f6b7e26531d523385b648
ryym <ryym.64@gmail.com>
2018-05-05 11:43:20 +0900

    chore(ts): Add Sinon.JS for mocking in tests

---
+++ frontend/@types/sinon/index.d.ts
@@ -0,0 +1,6 @@
+// TODO: Use proper type definitions.
+// Since Sinon.JS does not have official type definitions,
+// we need to use the one in DefinitelyTyped. But currently
+// it does not have definitions for Sinon.JS v5.
+
+export const fake: any;
