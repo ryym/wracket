c5d292b43cf7f5e24e697d6e9598a95b6395b3fc
ryym <ryym.64@gmail.com>
2018-05-05 11:57:08 +0900

    refac(api): Rewrite JS API client as functions instead of a class
    to make it easy to inject each API function separately into
    other functions which depends on it.

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,5 +1,20 @@
-import {Ajax, createAjax, Response} from './ajax';
+import {memoize} from './memoize';
+import {findCSRFToken} from './csrf-token';
+import {Ajax, createAjax} from './ajax';
 import {Bookmark, BookmarkById, SearchCondition, UnixTime} from '../lib/models';
+
+const getCSRFToken = memoize(() => {
+  const token = findCSRFToken();
+  if (token == null) {
+    throw new Error('CSRF token not found');
+  }
+  return token;
+});
+
+const createDefaultAjax = (csrfToken: string = getCSRFToken()): Ajax =>
+  createAjax({baseURL: '/api/', headers: {'X-CSRF-Token': csrfToken}});
+
+const getAjax = memoize(createDefaultAjax);
 
 export interface SearchResult {
   bookmarks: BookmarkById;
@@ -10,77 +25,71 @@ export interface FavoriteResult {
   favoritedAt: UnixTime;
 }
 
-const createDefaultAjax = (csrfToken: string): Ajax =>
-  createAjax({baseURL: '/api/', headers: {'X-CSRF-Token': csrfToken}});
-
-export class API {
-  private ajax: Ajax;
-
-  constructor(csrfToken: string, ajax: Ajax = createDefaultAjax(csrfToken)) {
-    this.ajax = ajax;
+export async function synchronize(
+  cdtn: SearchCondition,
+  ajax = getAjax(),
+): Promise<BookmarkById | null> {
+  const res = await ajax<BookmarkById>('/bookmarks/sync', {
+    method: 'put',
+    params: cdtn,
+  });
+  if (!res.isSuccess) {
+    throw new Error(`failed to synchronize bookmarks: ${res}`);
   }
-
-  async ping(name: string): Promise<Response<{hello: string}>> {
-    return await this.ajax<{hello: string}>('/ping', {method: 'get', params: {name}});
-  }
-
-  async synchronize(cdtn: SearchCondition): Promise<BookmarkById | null> {
-    const res = await this.ajax<BookmarkById>('/bookmarks/sync', {method: 'put', params: cdtn});
-    if (!res.isSuccess) {
-      throw new Error(`failed to synchronize bookmarks: ${res}`);
-    }
-    return res.data;
-  }
-
-  async search(cdtn: SearchCondition, last: Bookmark | null = null): Promise<SearchResult | null> {
-    let params: {} = cdtn;
-    if (last != null) {
-      params = {...params, offset: last.addedAt};
-    }
-    const res = await this.ajax<SearchResult>('/bookmarks/search', {params});
-    if (!res.isSuccess) {
-      throw new Error(`failed to search bookmarks: ${res}`);
-    }
-    return res.data;
-  }
-
-  async openBookmark(bookmarkId: string): Promise<{} | null> {
-    const res = await this.ajax<{}>(`/bookmarks/${bookmarkId}/open`, {method: 'put'});
-    if (!res.isSuccess) {
-      throw new Error(`failed to mark bookmark ${bookmarkId} as opened: ${res}`);
-    }
-    return {};
-  }
-
-  async resetOpenBookmark(bookmarkId: string): Promise<{} | null> {
-    const res = await this.ajax<{}>(`/bookmarks/${bookmarkId}/reset_open`, {method: 'put'});
-    if (!res.isSuccess) {
-      throw new Error(`failed to reset bookmark ${bookmarkId} open: ${res}`);
-    }
-    return {};
-  }
-
-  async favoriteBookmark(bookmarkId: string): Promise<FavoriteResult | null> {
-    const res = await this.ajax<FavoriteResult | null>(`/bookmarks/${bookmarkId}/favorite`, {
-      method: 'put',
-    });
-    if (!res.isSuccess) {
-      throw new Error(`failed to favorite bookmark ${bookmarkId}: ${res}`);
-    }
-    return res.data;
-  }
-
-  async unfavoriteBookmark(bookmarkId: string): Promise<{} | null> {
-    const res = await this.ajax<{} | null>(`/bookmarks/${bookmarkId}/unfavorite`, {
-      method: 'put',
-    });
-    if (!res.isSuccess) {
-      throw new Error(`failed to unfavorite bookmark ${bookmarkId}: ${res}`);
-    }
-    return {};
-  }
+  return res.data;
 }
 
-export function createAPI(csrfToken: string): API {
-  return new API(csrfToken);
+export async function search(
+  cdtn: SearchCondition,
+  last: Bookmark | null = null,
+  ajax = getAjax(),
+): Promise<SearchResult | null> {
+  let params: {} = cdtn;
+  if (last != null) {
+    params = {...params, offset: last.addedAt};
+  }
+  const res = await ajax<SearchResult>('/bookmarks/search', {params});
+  if (!res.isSuccess) {
+    throw new Error(`failed to search bookmarks: ${res}`);
+  }
+  return res.data;
+}
+
+export async function openBookmark(bookmarkId: string, ajax = getAjax()): Promise<{} | null> {
+  const res = await ajax<{}>(`/bookmarks/${bookmarkId}/open`, {method: 'put'});
+  if (!res.isSuccess) {
+    throw new Error(`failed to mark bookmark ${bookmarkId} as opened: ${res}`);
+  }
+  return {};
+}
+
+export async function resetOpenBookmark(bookmarkId: string, ajax = getAjax()): Promise<{} | null> {
+  const res = await ajax<{}>(`/bookmarks/${bookmarkId}/reset_open`, {method: 'put'});
+  if (!res.isSuccess) {
+    throw new Error(`failed to reset bookmark ${bookmarkId} open: ${res}`);
+  }
+  return {};
+}
+
+export async function favoriteBookmark(
+  bookmarkId: string,
+  ajax = getAjax(),
+): Promise<FavoriteResult | null> {
+  const res = await ajax<FavoriteResult | null>(`/bookmarks/${bookmarkId}/favorite`, {
+    method: 'put',
+  });
+  if (!res.isSuccess) {
+    throw new Error(`failed to favorite bookmark ${bookmarkId}: ${res}`);
+  }
+  return res.data;
+}
+
+export async function unfavoriteBookmark(bookmarkId: string, ajax = getAjax()): Promise<{} | null> {
+  const res = await ajax<{} | null>(`/bookmarks/${bookmarkId}/unfavorite`, {
+    method: 'put',
+  });
+  if (!res.isSuccess) {
+    throw new Error(`failed to unfavorite bookmark ${bookmarkId}: ${res}`);
+  }
+  return {};
 }
