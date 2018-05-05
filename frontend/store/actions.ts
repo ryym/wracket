d6abc5165b26a99b57dbe546c3f81973c1715fcc
ryym <ryym.64@gmail.com>
2018-05-05 11:30:05 +0900

    refac(js): Remove unnecessary sample action

--- frontend/store/actions.ts
+++ frontend/store/actions.ts
@@ -1,12 +1,2 @@
-import {thunk} from 'redux-dutiful-thunk';
-import {Thunk} from '../action';
-
 export * from './bookmarks/actions';
 export * from './errors/actions';
-
-export function ping(name: string): Thunk {
-  return thunk(async (_dispatch, _getState, {api}) => {
-    const res = await api.ping(name);
-    console.log('PONG', res.data);
-  });
-}
