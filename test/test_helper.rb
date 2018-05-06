aaa0a24a02b7fd8f408876e0dc7f1c2f40bd551a
ryym <ryym.64@gmail.com>
2018-05-06 09:37:19 +0900

    test(rb): Enable to use Mocky in test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,11 +4,15 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
 
+require Rails.root.join('lib', 'mocky')
+
 module ActiveSupport
   class TestCase
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
 
-    # Add more helper methods to be used by all tests here...
+    def mock_instance(clazz)
+      Mocky.instance_of(clazz)
+    end
   end
 end
