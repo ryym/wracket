22123ca20c1c02fb0c578d4a304e06f419e0b41f
ryym <ryym.64@gmail.com>
2018-07-14 23:31:03 +0900

    chore(rails): Stop colorizing log outputs
    ANSI color codes makes logs hard to read.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,6 +42,9 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
+  # Do not use ANSI color codes for logs.
+  config.colorize_logging = false
+
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
