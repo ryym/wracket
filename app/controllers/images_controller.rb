1911aa64f56a462470818d593af165388006aa94
ryym <ryym.64@gmail.com>
2018-08-06 09:52:06 +0900

    feat(image): Add controllers to process image ondemand

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class ImagesController < ViewBaseController
+  before_action do
+    @image_maker ||= ImageMaker.create
+  end
+
+  def transform
+    id, variant = params.permit(:id, :variant).values
+    return render status: :bad_request, plain: '' if variant.blank?
+
+    image = Image.find_by(id: id)
+    return render status: :not_found, plain: '' if image.nil?
+
+    variant_url = @image_maker.make_variant(image, variant)
+    redirect_to variant_url
+  end
+end
