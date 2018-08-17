9e662684492f916200d5bc369a41cef4d1447e5f
ryym <ryym.64@gmail.com>
2018-08-17 22:59:26 +0900

    fix(session): Specify cookie expiration to persist it
    It seems that a cookie without expiration disappears when the browser is
    closed.
    But I could not an official document about how to set an expiration to
    the session cookie.
    
    https://stackoverflow.com/questions/44859453/ruby-on-rails-session-expire-date

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,5 @@
 # https://github.com/rails/activerecord-session_store
-Rails.application.config.session_store :active_record_store
+Rails.application.config.session_store(
+  :active_record_store,
+  expire_after: 14.days,
+)
