1ecb9fa9624c53566191de7458413ad31371896e
ryym <ryym.64@gmail.com>
2018-09-25 23:27:22 +0900

    fix: typo

--- app/controllers/api/api_base_controller.rb
+++ app/controllers/api/api_base_controller.rb
@@ -27,7 +27,7 @@ module Api
 
     if Rails.env.production?
       def handle_server_error(_err)
-        render status: internal_server_error, json: {
+        render status: :internal_server_error, json: {
           message: 'Internal server error',
         }
       end
