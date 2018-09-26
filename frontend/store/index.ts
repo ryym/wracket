4bd1ebf86449779945c66bb80db5bdbb9796c256
ryym <ryym.64@gmail.com>
2018-09-27 07:59:55 +0900

    feat(js): Do not output Redux logs in production

--- frontend/store/index.ts
+++ frontend/store/index.ts
@@ -1,6 +1,7 @@
 import {createStore, applyMiddleware, Store as ReduxStore, Dispatch as ReduxDispatch} from 'redux';
 import logger from 'redux-logger';
 import {createThunkMiddleware} from 'redux-dutiful-thunk';
+import {IS_DEVELOPMENT} from '../consts';
 import {State} from '../state';
 import {Action} from '../action';
 import {rootReducer} from './reducer';
@@ -15,15 +16,15 @@ export type StoreConfig = {
   initialState?: Partial<State>;
 };
 
+const getMiddlewares = (isDev = IS_DEVELOPMENT) => {
+  const common = [errorCatchMiddleware(), enableBookmarkSearch(), createThunkMiddleware()];
+  return isDev ? [logger, ...common] : common;
+};
+
 export function configureStore(conf: StoreConfig): Store {
   return createStore<State, Action, {}, {}>(
     rootReducer,
     conf.initialState || {},
-    applyMiddleware(
-      logger,
-      errorCatchMiddleware(),
-      enableBookmarkSearch(),
-      createThunkMiddleware(),
-    ),
+    applyMiddleware(...getMiddlewares()),
   );
 }
