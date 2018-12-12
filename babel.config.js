5e02489d7dbf8c99952de15676391accc82312ea
ryym <ryym.64@gmail.com>
2018-12-12 14:01:54 +0900

    chore: Put browserslistrc config file
    to allow other tools to use it.
    I also tweaked its content.

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,6 @@
 /* eslint-env node */
 
 module.exports = {
-  presets: [
-    '@babel/preset-react',
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          browsers: ['>0.25%,  not op_mini all'],
-        },
-      },
-    ],
-  ],
-
+  presets: ['@babel/preset-react', '@babel/preset-env'],
   plugins: ['@babel/plugin-proposal-class-properties'],
 };
