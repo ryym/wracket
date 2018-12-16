05ce8651899407749791fa97c77a4f2c329f7ee4
ryym <ryym.64@gmail.com>
2018-12-16 17:51:11 +0900

    feat(js): Add util to generate search condition from query

--- frontend/lib/search-query.ts
+++ frontend/lib/search-query.ts
@@ -1,5 +1,29 @@
-import {SearchCondition} from './models';
+import {SearchCondition, StatusFilter} from './models';
 
 export const conditionToQuery = (cdtn: SearchCondition): string => {
-  return `&statusFilter=${cdtn.statusFilter}`;
+  return `statusFilter=${cdtn.statusFilter}`;
+};
+
+export const queryToCondition = (query: string): SearchCondition => {
+  if (query.startsWith('?')) {
+    query = query.substr(1);
+  }
+
+  let statusFilter: StatusFilter = StatusFilter.New;
+  query.split('&').forEach(part => {
+    const [key, value] = part.split('=');
+    if (key === 'statusFilter') {
+      let filter = convertToStatusFilter(value);
+      if (filter != null) {
+        statusFilter = filter;
+      }
+    }
+  });
+
+  return {statusFilter};
+};
+
+const convertToStatusFilter = (filter: string): StatusFilter | null => {
+  const values = Object.keys(StatusFilter).map((key: any) => StatusFilter[key]) as StatusFilter[];
+  return values.find(v => v === filter) || null;
 };
