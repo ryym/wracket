bf79d5b2ec8a84bcf7231b8b2329cd613f14560e
ryym <ryym.64@gmail.com>
2019-04-23 13:43:22 +0900

    test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test 'valids' do
-    user = User.new(username: 'alice', access_token: 'abc123')
+    user = User.new(username: nil, access_token: 'abc123')
     assert user.valid?
   end
 end
